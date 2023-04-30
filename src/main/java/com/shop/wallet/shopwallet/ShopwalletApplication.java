340ad7ed5a348729b1ccecbc955b32f9f59edafd
shreejan01 <shreejan.lohar@gmail.com>
2023-04-30 17:34:46 +0545

    pom file fixed

--- src/main/java/com/shop/wallet/shopwallet/ShopwalletApplication.java
+++ src/main/java/com/shop/wallet/shopwallet/ShopwalletApplication.java
@@ -1,9 +1,11 @@
 package com.shop.wallet.shopwallet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class ShopwalletApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/shop/wallet/shopwallet/entity/Account.java
+++ src/main/java/com/shop/wallet/shopwallet/entity/Account.java
@@ -1,4 +1,21 @@
 package com.shop.wallet.shopwallet.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account_tbl")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
 }
