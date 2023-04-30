61dd50fb614770c7996975cf24fd8254bcbe9022
shreejan01 <shreejan.lohar@gmail.com>
2023-04-30 14:37:20 +0545

    commit after account merge to main

--- src/main/java/com/shop/wallet/shopwallet/repository/AccountRepository.java
+++ src/main/java/com/shop/wallet/shopwallet/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.shop.wallet.shopwallet.repository;
 
+import com.shop.wallet.shopwallet.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
