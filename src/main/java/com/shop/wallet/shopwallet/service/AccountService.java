f1462c0ec27a55e0cc1086abaee414c85a93e257
shreejan01 <shreejan.lohar@gmail.com>
2023-05-02 10:32:30 +0545

    create account service methods

--- src/main/java/com/shop/wallet/shopwallet/service/AccountService.java
+++ src/main/java/com/shop/wallet/shopwallet/service/AccountService.java
@@ -1,4 +1,9 @@
 package com.shop.wallet.shopwallet.service;
 
+import com.shop.wallet.shopwallet.entity.Account;
+
 public interface AccountService {
+
+    //method to add account
+    Account addAccount();
 }
